0d3e5772a14c882afbec161f0be1f861cc6279bd
Brian Clark <bgclarrk@live.com>
2021-05-15 10:09:47 -0700

    Add cigar_id column to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_215432) do
+ActiveRecord::Schema.define(version: 2021_05_15_170725) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "review_title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_215432) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "cigar_id"
   end
 
   create_table "users", force: :cascade do |t|
