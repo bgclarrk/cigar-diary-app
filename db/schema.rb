4afc5987fda25de15d41b469f0a89a7700311f56
Brian Clark <bgclarrk@live.com>
2021-05-16 12:07:54 -0700

    Update to show page and added a cigar name to reviews db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_170725) do
+ActiveRecord::Schema.define(version: 2021_05_16_015635) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "review_title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_170725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "cigar_id"
+    t.string "review_cigar"
   end
 
   create_table "users", force: :cascade do |t|
